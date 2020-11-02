91515fcbcbf64a2c93d29630a1069558465bd441
Aatthitthae <aathitthan11@gmail.com>
2020-11-02 12:14:35 +1100

    update

---
+++ Custom Video Player/script.js
@@ -0,0 +1,35 @@
+// Get our Elements
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress_filled');
+
+const toggle = player.querySelector('.toggle');
+const skipButton = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player_slider');
+
+
+
+// Build Our Functions 
+function togglePLay() {
+    if (video.paused){
+        video.play();
+    } else {
+        video.pause();
+    }
+}
+
+function updateButton(){ 
+    const icon = this.pause ? '►' : '❚❚';
+    console.log('Update the button');
+    toggle.textContent = icon;
+}
+
+
+
+// Hook up the Event Listeners
+video.addEventListener('click', togglePLay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+
+toggle.addEventListener('click', togglePLay);
